8ba72eb7b2658f7faabf60ab54fb1d83b136e749
laxman <luxmanroyluxman@gmail.com>
2023-09-01 01:08:50 +0600

    new file created

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+const element = document.getElementById('root'),
+    root = ReactDOM.createRoot(element);
+
+
+root.render(
+    <App />
+)
