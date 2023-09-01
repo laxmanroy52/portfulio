ef256f2e0f3b27c2ffd31ebd6093cb8793386065
laxman <luxmanroyluxman@gmail.com>
2023-09-01 09:38:54 +0600

    Add laxman text with react and vite project

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="logo react" alt="React" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Laxman</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
           count is = {count}
